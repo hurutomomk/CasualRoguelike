303ee7d6734b75b5e2c4f6813dca1a793f510f3b
ChoiJihun <hurutomomk@gmail.com>
2024-02-26 13:25:51 +0900

    (0007)EvenetManager.csにEventセッティングのタイミングを追加。

--- Assets/Scripts/Game/Map/MapEventManager.cs
+++ Assets/Scripts/Game/Map/MapEventManager.cs
@@ -1,18 +1,93 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapEventManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    
+    #region [func]
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    private void Start()
     {
-        
+        // 破棄不可
+        DontDestroyOnLoad(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    #region [01. Setting Events]
+
+    /// <summary>
+    /// Map上にMapEventを生成
+    /// </summary>
+    /// <param name="onFinished"></param>
+    public void SetEvent(Action onFinished)
+    {
+        // ExitDoorを生成
+        this.SetExitDoor(() =>
+        {
+            // DoorKeyを生成
+            this.SetDoorKey(() =>
+            {
+                // Shrineを生成
+                this.SetShrine(() =>
+                {
+                    onFinished?.Invoke();
+                });
+            });
+        });
+    }
+
+
+
+    #region [001. SetExitDoor]
+    /// <summary>
+    /// ExitDoorを生成
+    /// </summary>
+    /// <param name="onFinished"></param>
+    private void SetExitDoor(Action onFinished)
     {
         
+
+        onFinished?.Invoke();
     }
+
+    #endregion
+    
+    
+    
+    #region [002. SetDoorKey]
+    /// <summary>
+    /// DoorKeyを生成
+    /// </summary>
+    /// <param name="onFinished"></param>
+    private void SetDoorKey(Action onFinished)
+    {
+        
+
+        onFinished?.Invoke();
+    }
+
+    #endregion
+    
+    
+    
+    #region [003. SetShrine]
+    /// <summary>
+    /// Shrineを生成
+    /// </summary>
+    /// <param name="onFinished"></param>
+    private void SetShrine(Action onFinished)
+    {
+        
+
+        onFinished?.Invoke();
+    }
+    #endregion
+
+    #endregion
+    
+    #endregion
 }
