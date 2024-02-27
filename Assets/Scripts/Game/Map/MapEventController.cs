db6380b5995f529206b023fc73d898ef76e6301b
ChoiJihun <hurutomomk@gmail.com>
2024-02-27 11:05:07 +0900

    (0007)MapEventController.csにMapEventPrefab用のソースコードを追加。

--- Assets/Scripts/Game/Map/MapEventController.cs
+++ Assets/Scripts/Game/Map/MapEventController.cs
@@ -1,18 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapEventController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region [var]
+
+    [Header(" --- Map Event Common")]
+    /// <summary>
+    /// ScriptableIbject上のMapEvent情報 
+    /// </summary>
+    [SerializeField]
+    private MapEvent mapEvent;
+    public MapEvent MapEvent { get => this.mapEvent; }
+    /// <summary>
+    /// EventPrefabの画像表示
+    /// </summary>
+    [SerializeField]
+    private SpriteRenderer eventSprite;
+    /// <summary>
+    /// EventFinished時適応するEventSpriteのアルファ値
+    /// </summary>
+    [SerializeField]
+    private float eventSpriteFinishedAlpha = 0.35f;
+
+    #endregion
+
+
+    #region [func]
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    private void Awake()
     {
-        
+        this.eventSprite.sprite = this.mapEvent.eventSprite_Start;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Event終了後、表示画像を終了Stateのものに変更
+    /// </summary>
+    public void SetSpriteToFinishedSprite()
     {
+        // MapEventがExitDoorの場合は処理しない
+        if (this.mapEvent.name.ToUpper() == "EXITDOOR")
+            return;
         
+        // 画像および色を変更
+        this.eventSprite.sprite = mapEvent.eventSprite_Finished;
+        var color = this.eventSprite.color;
+        this.eventSprite.color = new Color(color.r, color.g, color.b, this.eventSpriteFinishedAlpha);
     }
+    
+    /// <summary>
+    /// DoorKey取得後、ExitDoorの表示画像を変更
+    /// </summary>
+    public void SetExitDoorSpriteToFinishedSprite()
+    {
+        this.eventSprite.sprite = mapEvent.eventSprite_Change;
+    }
+
+    #endregion
 }
